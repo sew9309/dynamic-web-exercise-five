722f3ba8642b7089998a9e15c170bd0e096ab198
Administrator <administrator@10-18-198-58.dynapool.wireless.nyu.edu>
2023-11-09 09:54:38 -0500

    Thursday Nov 9 class

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+// import express from 'express';
+const app = express();
+
+
 const express = require('express')
 const router = express.Router();
 // Initialize firebase
---
+++ routes/singlePost.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+// Initialize firebase
+const firestore = require("firebase/firestore");
+// Create a reference to the database
+const db = firestore.getFirestore();
+
+router.get('/:postId', (req, res) => {
+    const postId = req.params.postId;
+    const postQuery = firestore.getDoc(firestore.doc(db, "posts", postId));
+    
+    postQuery
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+});
